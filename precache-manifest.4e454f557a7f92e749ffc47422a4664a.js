2827853bd3333c59c35995b7b5ff8704c3c3051b
Victor <dpntvictor@gmail.com>
2022-01-18 20:06:41 +0100

    Updates

---
+++ precache-manifest.4e454f557a7f92e749ffc47422a4664a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690da96d430e67ae001de8f8f0667228",
+    "url": "/Caseabo/index.html"
+  },
+  {
+    "revision": "7dac53c40c1cfffdeffc",
+    "url": "/Caseabo/static/css/main.f0667843.chunk.css"
+  },
+  {
+    "revision": "ccb41e57ffe731d9cda5",
+    "url": "/Caseabo/static/js/2.3054df81.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Caseabo/static/js/2.3054df81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dac53c40c1cfffdeffc",
+    "url": "/Caseabo/static/js/main.cad5c0f3.chunk.js"
+  },
+  {
+    "revision": "8dbcbe9a382910d79187",
+    "url": "/Caseabo/static/js/runtime-main.7b28edcf.js"
+  }
+]);
